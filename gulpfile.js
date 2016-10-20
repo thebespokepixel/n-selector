2d3a336e2cb6972f7eb1a3a0dfb730d47b69bf7e
Mark Griffiths <mark@thebespokepixel.com>
2016-10-20 16:50:40 +0100

    Add docs

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,28 @@ const cordial = require('@thebespokepixel/cordial')()
 
 // transpilation/formatting
 gulp.task('bundle', cordial.macro({
-	source: 'src/lib/index.js'
+	source: 'src/index.js'
 }).bundle())
 
 gulp.task('master', cordial.macro({
 	master: true,
-	source: 'src/lib/index.js'
+	source: 'src/index.js'
 }).bundle())
 
 // Clean
 gulp.task('clean', cordial.shell({
 	source: ['npm-debug.log', './nyc_output', './test/coverage']
 }).trash())
+
+// Docs
+gulp.task('docs', cordial.shell({
+	source: 'npm run doc-build'
+}).job())
+
+// ReadMe
+gulp.task('readme', cordial.shell({
+	source: 'npm run readme'
+}).job())
 
 // Tests
 gulp.task('ava', cordial.test().ava(['test/*.js']))
@@ -29,3 +39,10 @@ gulp.task('test', gulp.parallel('xo', 'ava'))
 
 // Default
 gulp.task('default', gulp.series('bump', 'bundle'))
+
+// Hooks
+gulp.task('start-release', gulp.series('reset', 'clean', 'master', 'readme'))
+gulp.task('post-flow-release-start', gulp.series('start-release', 'version-release', 'docs', 'commit'))
+
+// Default
+gulp.task('default', gulp.series('bump', 'clean', gulp.parallel('docs', 'bundle', 'readme')))
